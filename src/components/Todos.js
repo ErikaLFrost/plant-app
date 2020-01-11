78b826368a3f868d01f1100d34f8a1c7fbd52a2f
Erika Loman <erikaloman@nacka-kulturcentrums-macbook-pro-7.local>
2020-01-11 20:48:54 +0100

    Removed emojis

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -4,7 +4,7 @@ import { NewItem, ItemList } from "./Items";
 export default function Todo() {
   return (
     <header className="App-header">
-      <h2>🚀 ToDo App</h2>
+      <h2>ToDo App</h2>
       <NewItem />
       <ItemList />
     </header>
