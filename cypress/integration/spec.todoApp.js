25bf0f1919c7de50240c398cdc8a8a623d877ccd
Erika Loman <erikaloman@nacka-kulturcentrums-macbook-pro-7.local>
2020-01-15 14:12:18 +0100

    Added more Cypress tests

--- cypress/integration/spec.todoApp.js
+++ cypress/integration/spec.todoApp.js
@@ -6,6 +6,11 @@ describe("Test Todo App", function() {
       .should("have.value", "Gå på promenad");
     cy.get("button").click();
     cy.get(".Item").should("contain", "Gå på promenad");
+    cy.get('.Item')
+      .contains("Gå på promenad")
+      .contains("Done")
+      .click();
+    cy.get(".Item").contains("Gå på promenad").should("not.exist");
     cy.get('p[name="clear-all"]').click();
     cy.get(".Item").should("not.exist");
   });
